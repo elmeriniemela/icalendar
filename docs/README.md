968efe5a022bd023429d755e352b91cd287f5cfe
Elmeri Niemelä <niemela.elmeri@gmail.com>
2019-02-26 16:24:07 +0200

    Autorefactor

---
+++ docs/README.md
@@ -0,0 +1,12 @@
+# Refactoring tool for shitty Aalto .ics
+
+Installation:
+
+* virtualenv -p python3 venv
+* source venv/bin/activate
+* pip install -r requirements.txt
+
+Usage:
+* `python3 refactor_ics <ics-file>`
+
+![alt text](https://raw.githubusercontent.com/thecodebasesite/icalendar/master/docs/icalendar.png)
--- refactor_ics.py
+++ refactor_ics.py
@@ -18,9 +18,72 @@ def mapCal():
             count += 1
     return mapping
 
+def auto_refactor():
+    import re
+    from datetime import datetime
+    current_year = datetime.now().year
+    exclude = [
+        str(current_year),
+        "Luento/",
+        "Lecture/",
+        "Föreläsning/",
+        "Harjoitukset/",
+        "Exercises/",
+        "Övningar/",
+        "/Luento",
+        "/Lecture",
+        "/Föreläsning",
+        "/Harjoitukset",
+        "/Exercises",
+        "/Övningar",
+    ]
 
-        
+    replacements = {
+        r" / .*? - ": " ",
+        r"Otakaari 1": "",
+        r"Kurssitentti/Course examination/Kurssitentti": "TENTTI",
+        r"  ": r" ",
 
+    }
+
+    results = {
+
+    }
+    current_summarys = list(mapCal().values())
+    for summary in current_summarys:
+        new_parts = []
+        parts = summary.split(',')
+        for part in parts:
+            for find, replace in replacements.items():
+                part = re.sub(find, replace, part)
+            for pattern in exclude:
+                if re.findall(pattern, part):
+                    break
+            else:
+                new_parts.append(part)
+
+        re_ordered = list(new_parts)
+        for index, new_part in enumerate(new_parts):
+            if re.findall(r' [A-Z][0-9]', new_part):
+                re_ordered.append(re_ordered.pop(index))
+        results.update({summary: ",".join(re_ordered).strip()})
+
+    for key, value in results.items():
+        for component in cal.walk():
+            if component.name == 'VEVENT':
+                if key == str(component['summary']):
+                    component['summary'] = value
+        print()
+        print("Original:")
+        print(key)
+        print("Refactored:")
+        print(value)
+        print()
+
+    with open(sys.argv[1].replace('.ics', '') + "_refactored.ics", 'wb') as f:
+        f.write(cal.to_ical())
+
+   
 def handle_input():
     numbers = input("Number to replace (separate with comma if multiple selection ie. '1,2,3,4...'):\n")
     replacement = input("Give replacement:\n")
@@ -39,12 +102,13 @@ def handle_input():
 
 def help():
     print(
-        "\nCommands:\n" +
+        "\nCommands:\n"
         "p = print open calendar\n"
-        "q = quit\n" +
-        "s = save to example.ics\n"+
-        "r = refactor 1 or multiple lines\n" +
-        "cls = clear screen (run 'cls' in terminal)"
+        "q = quit\n"
+        "s = save to example.ics\n"
+        "r = refactor 1 or multiple lines\n"
+        "a = autorefactor\n"
+        "cls = clear screen (run 'cls' in terminal)\n"
         "? = help\n"
     )
 help()
@@ -58,6 +122,8 @@ while True:
         f.close()
     elif command == 'r':
         handle_input()
+    elif command == 'a':
+        auto_refactor()
     elif command == 'p':
         print("\n" + getCurrentCal() + "\n")
     elif command == '?':
